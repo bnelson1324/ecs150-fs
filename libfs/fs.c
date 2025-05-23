7691ed92de55e7e1fcf7b3566306e30ea1818f1f
bnelson1324 <bnelson1324>
2025-05-22 22:44:00 -0700

    phase1 work

--- libfs/fs.c
+++ libfs/fs.c
@@ -9,47 +9,58 @@
 
 typedef struct __attribute__((__packed__)) {
     char signature[8];
-    uint16_t blockCount;
+    uint16_t totalBlkCount;
     uint16_t rootBlkIndex;
     uint16_t dataBlkStartIndex;
     uint16_t dataBlkCount;
     uint8_t fatBlkCount;
-    char padding[4079];
+    char _padding[4079];
 } superblock_t;
 
 typedef struct __attribute__((__packed__)) {
-    // TODO
-} fat_t;
-
-typedef struct __attribute__((__packed__)) {
-    // TODO
-} rootDir_t;
+    char fileName[FS_FILENAME_LEN];
+    uint32_t fileSize;
+    uint16_t firstDataBlkIndex;
+    char _padding[10];
+} rootFile_t;
 
 /* state */
 superblock_t *superblock;
-
+uint16_t *fat;
+rootFile_t *rootFiles;
 
 int fs_mount(const char *diskname) {
     // allocate buffers
     superblock = malloc(BLOCK_SIZE);
+    fat = malloc(BLOCK_SIZE * superblock->fatBlkCount);
+    rootFiles = malloc(BLOCK_SIZE);
 
     // load metadata
     if (block_disk_open(diskname) == -1) {
         perror("fs_mount: failure to open disk");
         return -1;
     }
-    if (block_read(BLOCK_SIZE, superblock) == -1) {
+    if (block_read(0, superblock) == -1) {
         perror("fs_mount: failure to read superblock");
         return -1;
     }
-    // TODO: load fat, root
+    for (size_t i = 0; i < superblock->fatBlkCount; i++) {
+        if (block_read(i + 1, &fat[i]) == -1) {
+            perror("fs_mount: failure to read FAT");
+            return -1;
+        }
+    }
+    if (block_read(superblock->rootBlkIndex, rootFiles) == -1) {
+        perror("fs_mount: failure to read root");
+        return -1;
+    }
 
     // err check
     if (memcmp(superblock->signature, "ECS150FS", 8) != 0) {
         perror("fs_mount: bad signature");
         return -1;
     }
-    if (superblock->blockCount != block_disk_count()) {
+    if (superblock->totalBlkCount != block_disk_count()) {
         perror("fs_mount: superblock block count mismatch");
         return -1;
     }
@@ -68,6 +79,10 @@ int fs_umount(void) {
      */
 
     /* TODO */
+
+    // TODO: dealloc buffers
+    // TODO: close disk
+
     return 0;
 }
 
